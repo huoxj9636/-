0951d2268fe768018efbc5f02385a23d82aada5a
hxj <1186925784@qq.com>
2018-07-05 18:48:46 +0800

    v1.01

--- login.py
+++ login.py
@@ -1,1 +1,10 @@
 nihoa,hello world
+
+
+多人开发:
+23.git clone 地址, 克隆远程的代码到本地
+24.git push, 推送到远程仓库
+25.git config --global  credential.helper cache 十五分钟有效期
+26.git config  credential.helper 'cache --timeout==3600' 一个小时有效期
+27.git config --global credential.helper store 长期有效
+28.git pull ,拉取远程代码到本地目录
